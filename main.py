a030b1b4014c081ad6b825c46a832e1993a51d4f
Gogasmerter <chebunin19@gmail.com>
2022-11-21 15:20:34 +0400

    Add name

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def main():
+def main(name):
     print('My first git program')
     print('And I change it every day')
+    print(f'Hello, {name}')
 
 
 if __name__ == '__main__':
-    main()
+    main('Masha')
